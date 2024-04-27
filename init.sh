816a9a2179da033b11d978861d5f75fdc165f8b9
run2go <run2gotv@gmail.com>
2024-04-27 17:40:17 +0200

    added color presets to build default PS1 prompt

--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+#default prompt
+#'\[\e[2m\][\t] \[\e[0;1;38;5;209m\]\u\[\e[0;2m\]@\[\e[0;1;38;5;215m\]\H\[\e[0;2m\]:\[\e[0;38;5;105m\]\w\[\e[0m\] \[\e[38;5;215m\]❯\[\e[0m\] '
+
 # Function definitions
 init_sh() {
     install_dependencies curl
@@ -9,87 +12,108 @@ init_sh() {
 }
 
 init_bash() {
-    install_dependencies curl bash vim bash jq git htop
+    install_dependencies curl bash vim bash sed jq git htop
 
+    curl -fsSL https://raw.githubusercontent.com/run2go/shell/main/vim/vimrc -o ~/.vimrc
     curl -fsSL https://raw.githubusercontent.com/run2go/shell/main/bash/bashrc -o ~/.bashrc
-    curl -fsSL https://raw.githubusercontent.com/run2go/shell/main/zsh/vimrc -o ~/.vimrc
     . ~/.bashrc
+
+    # Decide on machine type
+    case $2 in
+        1) prompt_user='[0;1;38;5;197m\]';; #root
+        2) prompt_user='[0;1;38;5;209m\]';; #user
+        *) echo "Invalid machine: '$2'";;
+    esac
+
+    # Decide on user type
+    case $1 in
+        1) prompt_host='[0;1;38;5;113m\]';; #local
+        2) prompt_host='[0;1;38;5;218m\]';; #vm
+        3) prompt_host='[0;1;38;5;215m\]';; #dev
+        4) prompt_host='[0;1;38;5;203m\]';; #prod
+        *) echo "Invalid user: '$1'";;
+    esac
+
+    # Construct new prompt
+    prompt="\[\e[2m\][\t] \[\e$prompt_user\u\[\e[0;2m\]@\[\e$prompt_host\H\[\e[0;2m\]:\[\e[0;38;5;105m\]\w\[\e[0m\] \[\e[38;5;215m\]❯\[\e[0m\] "
+    # Update PS1 prompt in bashrc file
+    sed -i "s/^\(DEFAULT_PROMPT\s*=\s*\).*\$/\1$prompt/" ~/.bashrc
 }
 
 init_zsh() {
-    install_dependencies curl bash vim zsh jq git htop coreutils g++ make build-essential
+    install_dependencies curl bash
 
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    #(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /root/.bashrc
-    #eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-    #brew install gcc
-
-    curl -fsSL https://raw.githubusercontent.com/run2go/shell/main/zsh/zshrc -o ~/.zshrc
-    curl -fsSL https://raw.githubusercontent.com/run2go/shell/main/zsh/zshenv -o ~/.zshenv
-    curl -fsSL https://raw.githubusercontent.com/run2go/shell/main/zsh/vimrc -o ~/.vimrc
-
-    brew install zsh
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    brew install yazi ffmpegthumbnailer unar jq poppler fd ripgrep fzf zoxide
-    brew tap homebrew/cask-fonts
-    brew install zsh-autocomplete
-    brew install powerlevel10k
-
-    git clone https://github.com/sachinsenal0x64/crystal-theme
-    mkdir ~/.config/yazi
-    cd crystal-theme
-    mv theme.toml ~/.config/yazi/
-    cd -
- 
-    echo "Recommended Font: https://github.com/romkatv/powerlevel10k?tab=readme-ov-file#meslo-nerd-font-patched-for-powerlevel10k"
-    
-    chsh -s $(which zsh)
-    eval zsh
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 }
 
 install_dependencies() {
     if [ -x "$(command -v apk)" ]; then
-        apk add --no-cache "$@"
+        if [ "$(id -u)" -eq 0 ]; then
+            apk add --no-cache "$@"
+        else
+            sudo apk add --no-cache "$@"
+        fi
     elif [ -x "$(command -v apt-get)" ]; then
-        apt-get install "$@"
+        if [ "$(id -u)" -eq 0 ]; then
+            apt-get install "$@"
+        else
+            sudo apt-get install "$@"
+        fi
     elif [ -x "$(command -v dnf)" ]; then
-        dnf install "$@"
+        if [ "$(id -u)" -eq 0 ]; then
+            dnf install "$@"
+        else
+            sudo dnf install "$@"
+        fi
     elif [ -x "$(command -v pacman)" ]; then
-        pacman -s "$@"
+        if [ "$(id -u)" -eq 0 ]; then
+            pacman -S "$@"
+        else
+            sudo pacman -S "$@"
+        fi
     elif [ -x "$(command -v zypper)" ]; then
-        zypper install "$@"
+        if [ "$(id -u)" -eq 0 ]; then
+            zypper install "$@"
+        else
+            sudo zypper install "$@"
+        fi
     else
         echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: '$@'">&2;
     fi
 }
 
-# Move to home dir
-cd ~
+# Sample: ./init.sh 2 1 2
+# Use transfer parameter if provided
+desired_shell=$1
+desired_machine=$2
+#desired_user=$3
 
-# Invoke sudo usage if missing
-if [ "$(id -u)" -ne 0 ]; then
-    exec sudo "$0" "$@"
-    exit $?
-fi
+# Determine desired shell
+while [ -z "$desired_shell" ]; do
+    echo "Shell selection: sh (1), bash (2), zsh (3)"
+    read desired_shell
+done
 
-# Use transfer parameter if given
-if [ ! -z "$1" ]; then
-    number=$1
+# Determine machine type
+while [ -z "$desired_machine" ]; do
+    echo "Server selection: local (1), vm (2), development (3), production (4)"
+    read desired_machine
+done
+
+# Determine if user is root
+if [ "$(id -u)" -eq 0 ]; then
+    desired_user=1
 else
-    # Prompt the user to enter a number
-    echo "Enter a number (1, 2, or 3):"
-    read number
-    if [ -z "$number" ]; then
-        number=1
-    fi
+    desired_user=2
 fi
-
-case $number in
-    1) init_sh ;;
-    2) init_bash ;;
-    3) init_zsh ;;
-    *) echo "Invalid input. Please enter 1, 2, or 3." ;;
-esac
 
 # Overwrite default profile file
 wget https://raw.githubusercontent.com/run2go/shell/main/profile -O ~/.profile
+
+# Initialize & configure shell
+case $desired_shell in
+    1) init_sh $desired_machine $desired_user;;
+    2) init_bash $desired_machine $desired_user;;
+    2) init_zsh;;
+    *) echo "Invalid shell selection: '$desired_shell'" ;;
+esac
